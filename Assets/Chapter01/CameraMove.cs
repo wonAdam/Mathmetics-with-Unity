5828e151312c64612ed4babb841118fa6630785c
wonAdam <ehdgus5500@gmail.com>
2020-12-27 16:24:57 +0900

    init & Chapter1&2

---
+++ Assets/Chapter01/CameraMove.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] KeyCode KeyForward;
+    [SerializeField] KeyCode KeyBack;
+    [SerializeField] KeyCode KeyLeft;
+    [SerializeField] KeyCode KeyRight;
+    [SerializeField] KeyCode KeyUp;
+    [SerializeField] KeyCode KeyDown;
+    [SerializeField] float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessKeyInput();
+        ProcessMouseInput();
+    }
+
+    private void ProcessKeyInput()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if(Input.GetKey(KeyForward))
+            direction += Time.deltaTime * moveSpeed * Vector3.forward;
+        if(Input.GetKey(KeyBack))
+            direction += Time.deltaTime * moveSpeed * Vector3.back;
+        if(Input.GetKey(KeyLeft))
+            direction += Time.deltaTime * moveSpeed * Vector3.left;
+        if(Input.GetKey(KeyRight))
+            direction += Time.deltaTime * moveSpeed * Vector3.right;
+        if(Input.GetKey(KeyUp))
+            direction += Time.deltaTime * moveSpeed * Vector3.up;
+        if(Input.GetKey(KeyDown))
+            direction += Time.deltaTime * moveSpeed * Vector3.down;
+
+        transform.Translate(direction, Space.Self);
+    }
+
+    private void ProcessMouseInput()
+    {
+        // mouse right click
+        if(Input.GetMouseButton(1))
+        {
+
+        }   
+    }
+}
---
+++ Assets/Chapter01/Chapter01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter01 : MonoBehaviour
+{
+    [SerializeField] GameObject capsule;
+    public  float targetAngle =  0f;
+
+    private GameObject sphere;
+    private float buttonDownTime;
+
+    public float sphereMagnitudeX = 2.0f;
+    public float sphereMagnitudeY = 3.0f;
+    public float sphereFrequency = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(string.Format("mousePosition ({0:f}, {1:f})", Input.mousePosition.x, Input.mousePosition.y));
+
+            targetAngle = GetRotationAngleByTargetPosition(Input.mousePosition);
+
+            capsule.transform.eulerAngles = new Vector3(0f,0f,targetAngle);
+        }
+    }
+
+    private float GetRotationAngleByTargetPosition(Vector3 mousePosition)
+    {
+        Vector3 selfScreenPoint = Camera.main.WorldToScreenPoint(capsule.transform.position);
+        Vector3 diff = mousePosition - selfScreenPoint;
+        Debug.Log(string.Format("diff: ({0:f}, {1:f})", diff.x, diff.y));
+        float angle = Mathf.Atan2(diff.x, diff.y) * Mathf.Rad2Deg;
+        Debug.Log(string.Format("angle: {0:f}", (-1f)*angle));
+
+        return (-1f)*angle;
+    }
+}
